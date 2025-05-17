b6434c907984c3e79450b9c0003e831ae4def937
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-17 20:40:44 +0530

    made a function which stores every todo in an array

---
+++ src/code/todos.js
@@ -0,0 +1,33 @@
+function todolist() {
+
+    todos = [
+        {
+            title: "title",
+            description: "lorem ipsum bhosda",
+            duedate: "22-02-20",
+            priority: 1,
+            status: 0,
+            project: "default"
+        },
+        {
+            title: "Linkedin Post",
+            description: "IDEA Heckathon linkedin post",
+            duedate: "17-05-25",
+            priority: 1,
+            status: 0,
+            project: "default"
+        },
+        {
+            title: "DSA",
+            description: "DSA Karle bhai varna job nahi milega",
+            duedate: "22-02-20",
+            priority: 1,
+            status: 0,
+            project: "default"
+        }
+    ]
+
+    return todos;
+}
+
+export default todolist;
