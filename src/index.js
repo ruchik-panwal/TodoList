63c038f26360866a90b06571383612e0616f3638
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-18 02:18:40 +0530

    commint to push

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import {todoDom, creatorFormDom} from "./code/domCreator";
-import removeTodo from "./code/editTodo";
+import {removeTodo, createTodo} from "./code/editTodo";
 import "./css/todo.css";
 import "./css/creatorForm.css";
 
 todoDom();
 removeTodo();
 creatorFormDom();
+createTodo();
