e38cfb19bf3376ca1f0a4043000166af3c02f767
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-18 00:34:08 +0530

    fixed indentation

--- src/code/todos.js
+++ src/code/todos.js
@@ -1,30 +1,30 @@
 
-    let todos = [
-        {
-            title: "title",
-            description: "lorem ipsum bhosda",
-            duedate: "22-02-20",
-            priority: 1,
-            status: 0,
-            project: "default"
-        },
-        {
-            title: "Linkedin Post",
-            description: "IDEA Heckathon linkedin post",
-            duedate: "17-05-25",
-            priority: 1,
-            status: 0,
-            project: "default"
-        },
-        {
-            title: "DSA",
-            description: "DSA Karle bhai varna job nahi milega",
-            duedate: "22-02-20",
-            priority: 1,
-            status: 0,
-            project: "default"
-        }
-    ]
+let todos = [
+    {
+        title: "title",
+        description: "lorem ipsum bhosda",
+        duedate: "22-02-20",
+        priority: 1,
+        status: 0,
+        project: "default"
+    },
+    {
+        title: "Linkedin Post",
+        description: "IDEA Heckathon linkedin post",
+        duedate: "17-05-25",
+        priority: 1,
+        status: 0,
+        project: "default"
+    },
+    {
+        title: "DSA",
+        description: "DSA Karle bhai varna job nahi milega",
+        duedate: "22-02-20",
+        priority: 1,
+        status: 0,
+        project: "default"
+    }
+]
 
 
 function todolist() {
