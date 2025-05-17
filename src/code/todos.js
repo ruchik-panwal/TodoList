1db2538ea266b0facb93e13c195d8b9683d2e25b
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-18 00:45:39 +0530

    added a array for form inputs and referenced it to ... wouldnt have too cause we are not going to change the values

--- src/code/todos.js
+++ src/code/todos.js
@@ -26,9 +26,39 @@ let todos = [
     }
 ]
 
+let forminputs = [
+    {
+        name: "Title",
+        type: "text",
+        placeholder: "What To Do?",
+        required: true
+    },
+    {
+        name: "Description",
+        type: "text",
+        placeholder: "Add a description...",
+        required: false
+    },
+    {
+        name: "Due Date",
+        type: "date",
+        placeholder: "",
+        required: true
+    },
+    {
+        name: "Priority",
+        type: "text",
+        placeholder: ["1", "2", "3", "4"],
+        required: true
+    }
+]
 
 function todolist() {
     return todos;
+}
+
+function formInput(){
+    return forminputs;
 }
 
 export default todolist;
