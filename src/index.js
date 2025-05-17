c10e265346889de1a1d3a94cdc1676cecbc32966
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-18 01:38:51 +0530

    Commiting to push

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import todoDom from "./code/domCreator";
+import {todoDom, creatorFormDom} from "./code/domCreator";
 import removeTodo from "./code/editTodo";
-import "./css/todo.css"
+import "./css/todo.css";
+import "./css/creatorForm.css";
 
 todoDom();
 removeTodo();
+creatorFormDom();
