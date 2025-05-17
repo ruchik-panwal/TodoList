ff2530b6d93d232430cb9382467894dbbea73ddf
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-17 22:06:56 +0530

    created simple dom for showing todos

--- src/code/domCreator.js
+++ src/code/domCreator.js
@@ -3,16 +3,29 @@ import todolist from "./todos";
 function todoDom(){
 
     const body = document.querySelector("body");
-    const todoWrapper = document.createElement("div");
+    const todosWrapper = document.createElement("div");
+    todosWrapper.className = "todosWrapper";
+    let count = 0;
+
 
     todolist().forEach((todo) => {
 
+
+        const todoDiv = document.createElement("div");
+        todoDiv.className = "todoDiv";
+
         for(const type in todo){
-            console.log(type);
-            
+            const typeDiv = document.createElement("div");
+            typeDiv.className = type;
+            typeDiv.textContent = todo[type];
+            todoDiv.appendChild(typeDiv);
         }
 
+        count++;
+        todosWrapper.appendChild(todoDiv);
     });
+
+    body.appendChild(todosWrapper);
 }
 
 export default todoDom;
