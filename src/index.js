f72f30c5a1faa7a44da4fced84e4efef5aa02b0d
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-21 00:57:41 +0530

    nothing just pushin

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import { todoDom, creatorFormDom, createNewBtn, defProjectBtn } from "./code/domCreator";
 import { removeTodo } from "./code/editTodo";
 import { newButton, editButton } from "./code/editControl";
-import { newProject } from "./code/projectSystem";
+import { newProject, projectSelection } from "./code/projectSystem";
 import "./css/todo.css";
 import "./css/creatorForm.css";
+import "./css/projectButtons.css"
 
 defProjectBtn();
 newProject();
+projectSelection();
 createNewBtn();
 newButton();
 todoDom();
