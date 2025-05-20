334826d34ccd0da9d4c5dcd5e7965be11b0161c7
ruchik-panwal <ruchik.panwal@gmail.com>
2025-05-20 21:15:19 +0530

    pushing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { todoDom, creatorFormDom, createNewBtn, defProjectBtn } from "./code/domCreator";
-import {removeTodo} from "./code/editTodo";
-import {newButton, editButton} from "./code/editControl";
-import newProject from "./code/projectSystem";
+import { removeTodo } from "./code/editTodo";
+import { newButton, editButton } from "./code/editControl";
+import { newProject } from "./code/projectSystem";
 import "./css/todo.css";
 import "./css/creatorForm.css";
 
